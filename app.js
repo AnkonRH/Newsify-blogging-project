916a3308d16a1d08f251d9b7f1b303a6da4568b3
AnkonRH <ankonrishavhaldar@gmail.com>
2025-07-24 21:20:24 +0530

    Prepare app for Vercel deployment

--- app.js
+++ app.js
@@ -38,6 +38,11 @@ app.use("/view", viewroutes);
 app.use("/comment", commentRoutes);
 app.use("/delete", checkforauthenticationcookie("token"), deleteRoutes);
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`);
-});
+if (require.main === module) {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  }
+  
+  module.exports = app;
+  
